b67a15bed75613de8e658a5c328a65ecf2342f95
Dany02S <77163542+Dany02S@users.noreply.github.com>
2024-04-30 20:27:23 +0300

    Server running MongoDB connection

---
+++ server/app.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const connection = require('./db')
+
+connection()
+app.use(express.json())
+app.use(cors())
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
