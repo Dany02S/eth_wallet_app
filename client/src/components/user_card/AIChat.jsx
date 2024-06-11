9ed494382195c0ca72382bf7c60c87ec65168b6b
Dany02S <77163542+Dany02S@users.noreply.github.com>
2024-06-11 11:19:50 +0300

    Modify: Custom AI chat done

--- client/src/components/user_card/AIChat.jsx
+++ client/src/components/user_card/AIChat.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { getAIAnswer } from "../../services/fetching";
 
 const AIChat = () => {
@@ -6,10 +6,41 @@ const AIChat = () => {
     const [history, setHistory] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState("");
+    const chatContainerRef = useRef(null);
 
-    // useEffect(() => {
+    useEffect(() => {
+        
+        const sendFirstMessage = async () => {
+            try {
+                const commanding = `
+                    Hello Gemini! From now on your name is EthaAI made by ETH Wallet company you will answer all the questions about cryptocurrencies and blockchain.
+                    If the questtion does not relate to cryptocurrencies, just answer: "I am sorry, I can only answer questions about cryptocurrencies."
+                    If you do not know the answer to the question, just answer: "I am sorry, I do not know the answer to that question."
+                    If the question is inappropriate, just answer: "I am sorry, I cannot answer that question."
+                    
+                    All of your answers should be plain text, no need for any formatting.
+                    All of your answers should be maximum 130 characters long.
+                    `
+                const response = await getAIAnswer(commanding, history);
+                setHistory([{
+                    role: "user",
+                    parts: [{text: commanding}]
+                },{
+                    role: "model",
+                    parts: [{text: response.message}]
+                }])
+            } catch (error) {
+                setError(error.message);
+            }
+        }
+        sendFirstMessage();
+    }, [])
 
-    // }, [])
+    useEffect(() => {
+        if (chatContainerRef.current) {
+            chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+        }
+    }, [history]);
 
     const sendMessage = async () => {
         setLoading(true);
@@ -19,18 +50,19 @@ const AIChat = () => {
             setTimeout(() => setError(""), 3000);
             return;
         } 
-        try {
-        const response = await getAIAnswer(question, history);
-        setHistory(oldChat => [...oldChat, {
-            role: "user",
-            parts: [{text: question}]
-        }, {
-            role: "model",
-            parts: [{text: response.message}]
-        }])
+        const mess = question;
         setQuestion("");
-
-        setLoading(false);
+        try {
+            setHistory(oldChat => [...oldChat, {
+                role: "user",
+                parts: [{text: mess}]
+            }])
+            const response = await getAIAnswer(mess + " (answer in maximum 6 sentences, and only plain text, no formatting)", history.slice(0, history.length - 1));
+            setHistory(oldChat => [...oldChat, {
+                role: "model",
+                parts: [{text: response.message}]
+            }])
+            setLoading(false);
         } catch (error) {
             setError(error.message);
         }
@@ -40,20 +72,20 @@ const AIChat = () => {
     return (
         <div className="ai-chat-container">
 
-            {history.length == 0 ?
+            {history.length <= 2 && !loading ?
             <div className="welcome-bar">
                 <img src="gemini.png" alt="" />
                 <h3>AI will answer your crypto questions</h3>
             </div> 
-            : <div className="ai-chat">
-                {history.map((chat, index) => (
+            : <div className="ai-chat" ref={chatContainerRef}>
+                {history.slice(2).map((chat, index) => (
                         <p key={index} className={chat.role === "model" ? "recieved-message" : "sent-message"}>{chat.parts[0].text}</p>
                 ))}
                 {loading && <img src="/gemini.png" className="loading-ai"  alt="loading" />}
             </div>}
             {error && <div className="form-error">{error}</div>}
             <div className="form-inputs">
-                <input type="text" className="form-input" placeholder="Type a message..." value={question} onChange={(e) => setQuestion(e.target.value)} />
+                <input type="text" className="form-input" id="inputFiled" placeholder="Type your question..." value={question} onChange={(e) => setQuestion(e.target.value)} />
                 <button className="form-button" onClick={sendMessage}>Send</button>
             </div>
         </div>
